c659156777bab2bc78686f523306e502214d750d
austinmchau <austin.matthew.chau@gmail.com>
2017-02-22 13:28:17 -0800

    Initialized the XCode Project

---
+++ neural-network-navigation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  neural-network-navigation
+//
+//  Created by Austin Chau on 2/22/17.
+//  Copyright © 2017 Psych186B. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ neural-network-navigation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  neural-network-navigation
+//
+//  Created by Austin Chau on 2/22/17.
+//  Copyright © 2017 Psych186B. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
