5f01dfe745c37188e08a9c6f03a53c179aea149f
austinmchau <austin.matthew.chau@gmail.com>
2017-02-24 11:14:38 -0800

    Changes 2

--- neural-network-navigation/CityGenerator.swift
+++ neural-network-navigation/CityGenerator.swift
@@ -10,28 +10,88 @@ import Foundation
 
 class City {
     
-    private var nodes = [Intersection]()
+    // Mark: Properties
+    private(set) var nodes = Set<Intersection>()
+    private(set) var area = (x: 0, y: 0)
+    
+    // Mark: Privte Properties for Computations
+    private var ids = Set<Int>()
     
     
-    func generate(size: Int) {
+    
+    // Mark: Methods
+    func generate(area: (x: Int, y: Int), nodeCount: Int) {
+        self.area = area
         
     }
     
-    private func generateRandomConnections(minConnections min: Int, maxConnections max: Int) {
-        for node in nodes {
-            let connectionCount = Int(arc4random_uniform(UInt32(max - min))) + min
-            
-        }
-    }
+    // Mark: Convenience Methods
     
+    private func random(min: Int = 0, max: Int = Int(UInt32.max)) -> Int {
+        return Int(arc4random_uniform(UInt32(max - min))) + min
+    }
     
 }
 
+/*
+ This extension deals with the backends for the generation of the city
+ */
+extension City {
+    private func generateConnections(intersection: Intersection, minConnections min: Int, maxConnections max: Int) {
+        for node in nodes {
+            let connectionCount = random(min: min, max: max)
+            for i in 0..<connectionCount {
+                
+            }
+        }
+    }
+    
+    private func generateIntersection() -> Intersection {
+        let id = generateIntersectionID()
+        let xCoor = random(min: 0, max: area.x)
+        let yCoor = random(min: 0, max: area.y)
+        
+    }
+    
+    private func generateRoad(intersection: Intersection) -> Road {
+        
+    }
+    
+    
+    private func generateIntersectionID() -> Int {
+        let id = random()
+        if ids.contains(id) {
+            ids.insert(id)
+            return id
+        } else {
+            return generateIntersectionID()
+        }
+    }
+
+}
+
+/*
+ This extension defines some classes and structs that are used by the class
+ */
 extension City {
     
-    struct Intersection {
+    class Intersection: Hashable {
+        // Hashable
+        var hashValue: Int { return id }
+        static func == (lhs: Intersection, rhs: Intersection) -> Bool {
+            return lhs.id == rhs.id || (lhs.coor.x == rhs.coor.x && lhs.coor.y == rhs.coor.y)
+        }
+        
+        // Class
+        let id: Int
         let coor: (x: Int, y: Int)
-        let roads: [Road]
+        var roads = [Road]()
+        
+        init(id: Int, coor: (x: Int, y: Int), roads: [Road]) {
+            self.id = id
+            self.coor = coor
+            self.roads = roads
+        }
     }
     
     struct Road {
