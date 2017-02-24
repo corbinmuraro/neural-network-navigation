03745e8becf9d38883cf2934816441dfca370186
Corbin Muraro <corbinmuraro@gmail.com>
2017-02-24 11:15:55 -0800

    Add NN framework

---
+++ neural-network-navigation/CityBuilder.swift
@@ -0,0 +1,19 @@
+//
+//  CityBuilder.swift
+//  neural-network-navigation
+//
+//  Created by Corbin Muraro on 2/24/17.
+//  Copyright © 2017 Psych186B. All rights reserved.
+//
+
+import Foundation
+
+class CityBuilder {
+
+    struct 
+    var intersections = [Double](count: 3, repeatedValue: 0.0)
+    
+    let network = FFNN(inputs: )
+
+
+}
