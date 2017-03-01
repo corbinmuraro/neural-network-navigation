5c202a7bcce41a68a133e54edd401a13da8549ec
austinmchau <austin.matthew.chau@gmail.com>
2017-03-01 09:58:16 -0800

    More UI Stuff

---
+++ neural-network-navigation/CustomWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  CustomWindowController.swift
+//  neural-network-navigation
+//
+//  Created by Austin Chau on 2/27/17.
+//  Copyright © 2017 Psych186B. All rights reserved.
+//
+
+import Cocoa
+
+class CustomWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        window?.titleVisibility = .hidden
+    }
+
+}
--- neural-network-navigation/Utils.swift
+++ neural-network-navigation/Utils.swift
@@ -36,4 +36,3 @@ extension NumberFormatter {
         }
     }
 }
-
