a760d7a7f621c0d82f5f948c4aaa87e9124fba7f
Corbin Muraro <corbinmuraro@gmail.com>
2017-03-01 11:26:40 -0800

    Build network input data set

--- neural-network-navigation/CityBuilder.swift
+++ neural-network-navigation/CityBuilder.swift
@@ -19,9 +19,9 @@ class CityBuilder {
     func build(cityOfSize cityArea: Vector2, nodeCount: Int, delegate: CityGeneratorDelegate?, completion: () -> Void) {
         city = City()
         city.generate(cityArea: cityArea, nodeCount: nodeCount, delegate: delegate)
-        var numNodes = city.nodes.count
+        let numNodes = city.nodes.count
         
-        //let network = FFNN(inputs: numNodes, hidden: 20, outputs: numNodes, learningRate: 1.0, momentum: 0.5, weights: nil, activationFunction: .Sigmoid, errorFunction: .crossEntropy(average: true))
+        let network = FFNN(inputs: numNodes, hidden: 20, outputs: numNodes, learningRate: 1.0, momentum: 0.5, weights: nil, activationFunction: .Sigmoid, errorFunction: .crossEntropy(average: true))
         
         
         // Calls the completion() callback for the caller
@@ -29,12 +29,26 @@ class CityBuilder {
     }
 
     
-    
-    func getTrainingData() {
-        
-    }
-    
     func trainNetwork() {
+        // build array (input array) of arrays (paths) of Vector2 intersection coordinates
+        var inputs = [[Vector2]]()
+        var answers = [[Vector2]]()
         
+        for i in city.nodes {
+            inputs.append(([i.coor]))
+//            answers.append(()) // append correct path
+        }
+            
+            
+//        let weights = try! network.train(inputs: inputs, answers: answers,
+//                                         testInputs: testInputs, testAnswers: testAnswers,
+//                                         errorThreshold: 0.1)
     }
+    
+
+}
+
+struct Coord {
+    var x: Int
+    var y: Int
 }
